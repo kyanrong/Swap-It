85703d94e226493e409606f153ce1ff3bc233b75
kyanrong <kekyanrong@gmail.com>
2015-02-05 11:15:46 +0800

    Edited functions in main.js and grid.js

--- grid.js
+++ grid.js
@@ -1,6 +1,7 @@
 // Stores information about the current state of the grid (10w x 20h)
 
-Grid = function(x, y) { 
+Grid = function(game, x, y) { 
+    this.game = game;
     // starting coords of the pieces
     this.piece_start_x = x; 
     this.piece_start_y = y;
@@ -16,10 +17,9 @@ Grid.prototype = {
         for(var i=0; i<this.arr.length; i++) {
             this.arr[i] = new Array(20);
         }
-        
+
         for(var i=0; i<this.arr.length; i++) {
             for(var j=0; j<this.arr[i].length; j++) {
-                var coord = translateToWorldCoord(i, j, this.piece_start_x);
                 this.arr[i][j] = 0;
             }
         }
@@ -28,16 +28,16 @@ Grid.prototype = {
     updateGrid: function(curr_piece, rowComplete, idx) {
         if(rowComplete) {
             // zero out complete row
-            for(var i=0; i<10; i++) {
+            /*for(var i=0; i<10; i++) {
                 this.arr[i][idx] = 0;
-            }
+            }*/
+            
             // shift blocks above idx down by one row
             for(var i=0; i<this.arr.length; i++) {
+                console.log('shift blocks down');
                 for(var j=0; j<idx; j++) {
-                    if(this.arr[i][j] == 1) {
-                        this.arr[i][j+1] = 1;
-                        this.arr[i][j] = 0;
-                    }
+                    this.arr[i][j+1] = this.arr[i][j];
+                    this.arr[i][j] = 0;
                 }
             }
         }
--- main.js
+++ main.js
@@ -12,6 +12,8 @@ var grid_yellow, grid_orange;                                       // stores th
 var grid_sprite_yellow, grid_sprite_orange;                         // stores the sprites in the current grid
 var curr_piece_a, curr_piece_b;                                     // a is always on the left, b on the right
 var next_lane_a, next_ps_a, next_lane_b, next_ps_b;                 // next lane, next piece shape
+var fall_time = 0, max_fall_time = 500;
+var p =0;
 
 function preload() {
     this.game.load.image('sprite_bg', 'assets/bg.png');
@@ -37,8 +39,8 @@ function create() {
     right_key = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
     down_key = this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
     
-    grid_yellow = new Grid(start_x1, start_y);
-    grid_orange = new Grid(start_x2, start_y);
+    grid_yellow = new Grid(this.game, start_x1, start_y);
+    grid_orange = new Grid(this.game, start_x2, start_y);
     
     grid_sprite_yellow = new Array();
     grid_sprite_orange = new Array();
@@ -46,11 +48,16 @@ function create() {
     // create tetris pieces for the 2 grids
     curr_piece_a = new Piece(this.game, start_x1, chooseLane(), choosePieceShape());
     curr_piece_b = new Piece(this.game, start_x2, chooseLane(), choosePieceShape());
+    //curr_piece_a = new Piece(this.game, start_x1, 0, 2);
     
-    next_lane_a = chooseLane();
-    next_ps_a = choosePieceShape();
+    //next_lane_a = chooseLane();
+    //next_ps_a = choosePieceShape();
     next_lane_b = chooseLane();
-    next_ps_b = choosePieceShape(); 
+    next_ps_b = choosePieceShape();
+    
+    p += 2;
+    next_lane_a = 2;
+    next_ps_a = p;
 }
 
 function update() {
@@ -58,31 +65,37 @@ function update() {
         swap();
     }
     
-    var curr_time = new Date().getTime();
-    // speed of falling piece
-    if(curr_time-prev_time > 500) {
+    if(down_key.isDown) {
+        max_fall_time = 50;
+    }
+    
+    if(this.game.time.now > fall_time) {
         
         // check the sprite positions of the grids
         // piece_a always on the left, piece_b always on the right
         if(onLeft(sprite_yellow)) {         // == sprite_orange on the right
             if(collide(curr_piece_a, grid_yellow)) {
                 grid_yellow.updateGrid(curr_piece_a, false, null);
+                checkCompletedRow(grid_yellow, grid_sprite_yellow);
                 renderUpdatedGrid(grid_yellow, grid_sprite_yellow);
                 destroyPiece(curr_piece_a);
-                checkCompletedRow(grid_yellow, grid_sprite_yellow);
                 
                 curr_piece_a = new Piece(this.game, start_x1, next_lane_a, next_ps_a);
                 next_lane_a = chooseLane();
                 next_ps_a = choosePieceShape();
+               /* p+=2;
+                next_lane_a = p;
+                next_ps_a = 2;*/
             }
             else {
                 curr_piece_a.fall();
             }
             if(collide(curr_piece_b, grid_orange)) {
                 grid_orange.updateGrid(curr_piece_b, false, null);
+                checkCompletedRow(grid_orange, grid_sprite_orange);
                 renderUpdatedGrid(grid_orange, grid_sprite_orange);
                 destroyPiece(curr_piece_b);
-                checkCompletedRow(grid_orange, grid_sprite_orange);
+                
                 
                 curr_piece_b = new Piece(this.game, start_x2, next_lane_b, next_ps_b);
                 next_lane_b = chooseLane();
@@ -97,9 +110,10 @@ function update() {
         else {
             if(collide(curr_piece_a, grid_orange)) {
                 grid_orange.updateGrid(curr_piece_a, false, null);
+                checkCompletedRow(grid_orange, grid_sprite_orange);
                 renderUpdatedGrid(grid_orange, grid_sprite_orange);
                 destroyPiece(curr_piece_a);
-                checkCompletedRow(grid_orange, grid_sprite_orange);
+                
                 
                 curr_piece_a = new Piece(this.game, start_x1, next_lane_a, next_ps_a);
                 next_lane_a = chooseLane();
@@ -110,9 +124,10 @@ function update() {
             }
             if(collide(curr_piece_b, grid_yellow)) {
                 grid_yellow.updateGrid(curr_piece_b, false, null);
+                checkCompletedRow(grid_yellow, grid_sprite_yellow);
                 renderUpdatedGrid(grid_yellow, grid_sprite_yellow);
                 destroyPiece(curr_piece_b);
-                checkCompletedRow(grid_yellow, grid_sprite_yellow);
+                
                 
                 curr_piece_b = new Piece(this.game, start_x2, next_lane_b, next_ps_b);
                 next_lane_b = chooseLane();
@@ -123,7 +138,9 @@ function update() {
             }
         }
 
-        prev_time = curr_time;
+        //prev_time = curr_time;
+        fall_time = this.game.time.now + max_fall_time;
+        max_fall_time = 500;
     }
 
 
@@ -186,6 +203,11 @@ function onLeft(sprite) {
 }
 
 function collide(curr_piece, grid) {
+    // collide with grid's base
+    if(curr_piece.base.y+block_len == start_y+block_len+grid_height) {
+        return true;
+    }
+    
     // collide with other pieces
     for(var i=0; i<curr_piece.blocks.length; i++) {
         var coord = translateToArrayCoord(curr_piece.blocks[i].x, curr_piece.blocks[i].y, grid.piece_start_x);
@@ -193,11 +215,7 @@ function collide(curr_piece, grid) {
             return true;
         }
     }
-    
-    // collide with grid's base
-    if(curr_piece.base.y+block_len == start_y+block_len+grid_height) {
-        return true;
-    }
+  
     return false;
 }
 
@@ -229,14 +247,17 @@ function resetBlockSprites() {
 
 function checkCompletedRow(grid, grid_sprite) {
     for(var j=19; j>-1; j--) {
-        for( var i=0; i<grid.arr.length; i++) {
-            if(grid.arr[i][j] == 0) {
+        for( var i=0; i<10; i++) {
+            //console.log(i,j);
+            if(grid.arr[i][j] == 0) {  
+                console.log('zero');
                 return;
             }
-            // current row is completed
-            grid.updateGrid(null, true, j)
-            resetBlockSprites();
-            renderUpdatedGrid(grid, grid_sprite);
         }
+        // current row is completed
+        console.log('current row completed');
+        grid.updateGrid(null, true, j)
+        resetBlockSprites();
+        //renderUpdatedGrid(grid, grid_sprite);
     }
 }
